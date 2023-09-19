54936c8eb4408c4e92893d4b07ea562743355ed9
Dominik Kawka <dominikkawka2002@gmail.com>
2023-09-19 18:49:03 +0100

    remove product by id, implemented and tested

---
+++ week2/src/productCatalogue.js
@@ -0,0 +1,41 @@
+class Catalogue {
+  constructor(title) {
+    this.title = title;
+    this.products = [];
+  }
+
+  findProductById(id) {
+    const match = this.products.find(
+      function (product) {
+        return id === product.id;
+      })
+    return match;
+  }
+
+  addProduct(product) {
+    if (!this.findProductById(product.id)) {
+      this.products.push(product);
+      return true;
+    }
+    return false;
+  }
+
+  findProductsByNameLike(subString) {
+    const matches = this.products.filter(function(product) {
+      const position = product.name.search(subString)
+      return position !== -1
+    } )
+    return matches;
+  }
+
+  removeProductById(id) {
+      if (this.findProductById(id)) {
+      this.products.splice(id, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+}
+module.exports = Catalogue;
